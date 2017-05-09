b673d068b97d886e95f8742faf6d98b9f7df3928
Jun Ho Park <pjh374968@gmail.com>
2017-05-09 11:18:43 +0900

    Unify controller, Modify return type of controller

--- src/main/java/com/springbook/view/user/LoginController.java
+++ src/main/java/com/springbook/view/user/LoginController.java
@@ -3,11 +3,14 @@ package com.springbook.view.user;
 import com.springbook.biz.user.UserVO;
 import com.springbook.biz.user.impl.UserDAO;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Created by Jun Ho Park on 2017-05-07.
@@ -15,7 +18,7 @@ import javax.servlet.http.HttpServletResponse;
 @Controller
 public class LoginController {
 
-    @RequestMapping("/login.do")
+    /*@RequestMapping("/login.do")
     public String login(UserVO vo, UserDAO userDAO) {
         System.out.println("로그인 처리");
 
@@ -24,24 +27,30 @@ public class LoginController {
         else
             return "login.jsp";
 
-        /*String id = request.getParameter("id");
-        String password = request.getParameter("password");
 
-        UserVO vo = new UserVO();
-        vo.setId(id);
-        vo.setPassword(password);
+    }*/
 
-        UserDAO userDAO = new UserDAO();
+    @RequestMapping(value="/login.do", method = RequestMethod.GET)
+    public String loginView(@ModelAttribute("user") UserVO vo) {
+        System.out.println("로그인 화면으로 이동");
+        vo.setId("test");
+        vo.setPassword("test123");
+        return "login.jsp";
+    }
+
+    @RequestMapping(value="/login.do", method = RequestMethod.POST)
+    public String login(UserVO vo, UserDAO userDAO, HttpSession session) {
+        System.out.println("로그인 인증 처리");
+        /*if(userDAO.getUser(vo) != null)
+            return "getBoardList.do";
+        else
+            return "login.jsp";*/
         UserVO user = userDAO.getUser(vo);
-
-        ModelAndView mav = new ModelAndView();
         if(user!=null) {
-            System.out.print("wpeofhefmdjah");
-            mav.setViewName("redirect:getBoardList.do");
+            session.setAttribute("userName", user.getName());
+            return "getBoardList.do";
         } else {
-            mav.setViewName("redirect:login.jsp");
+            return "login.jsp";
         }
-
-        return mav;*/
     }
 }
